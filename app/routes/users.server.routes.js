a5462f788db3d444f71ba7fb8029ed96e7a38b24
flubin <flubin@intellicheck.com>
2016-11-23 09:51:37 -0500

    Add READ functionality for users

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,5 +1,15 @@
 var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
-    app.route('/users').post(users.create);
-}
+    app.route('/users')
+    .post(users.create)
+    .get(users.list);
+
+    app.route('/users/:userId')
+    .get(users.read);
+
+// app.param method defines a middleware to be executed before any other middleware
+// that uses that parameter.
+    app.param('userId', users.userByID);
+};
+
