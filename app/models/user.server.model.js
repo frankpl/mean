71dbceed1dc055d40fb844c3075cc1cbf700c8f4
flubin <flubin@intellicheck.com>
2016-12-04 18:01:08 -0500

    Add local authentication strategy, updated user model

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+    crypto = require('crypto'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -7,21 +8,32 @@ var UserSchema = new Schema({
     email: {
         type: String,
         index: true,
-        match: /.+\@.+\..+/
+        match: [/.+\@.+\..+/, "please fill in a valid email address"]
     },
     username: {
         type:String,
+        unique: true,
+        required: 'Username is required',
         trim: true
     },
     password: {
         type: String,
         validate: [
             function(password) {
-                return password.length >= 6;
+                return password && password.length >= 6;
             },
             'Password is too short'
         ]
     },
+    salt: {
+        type: String
+    },
+    provider: {
+        type: String,   
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
     created: {
         type: Date,
         default: Date.now
@@ -40,9 +52,7 @@ var UserSchema = new Schema({
         }
     }
 });
-UserSchema.statics.findOneByUsername = function (username, callback) {
-    this.findOne( {username: new RegExp(username, 'i')}, callback);
-};
+
 UserSchema.virtual('fullName').get(function() {
     return this.firstName + ' ' + this.lastName;
 }).set(function(fullName) {
@@ -50,5 +60,46 @@ UserSchema.virtual('fullName').get(function() {
     this.firstName = splitName[0] || '';
     this.lastName = splitName[1] || '';
 });
+
+UserSchema.pre('save', function(next) {
+    if (this.password) {
+        this.salt = new
+            Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+    return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findOneByUsername = function (username, callback) {
+    this.findOne( {username: new RegExp(username, 'i')}, callback);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+
+    _this.findOne({
+        username: possibleUsername
+    }, function(err, user) {
+        if (!err) {
+            if (!user) {
+                callback(possibleUsername);
+            } else {
+                return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+            }
+        } else {
+            callback(null);
+        }
+    });
+};
+
 UserSchema.set('toJSON', {getters: true, virtual: true});
 mongoose.model('User', UserSchema);
--- config/express.js
+++ config/express.js
@@ -7,6 +7,7 @@ var path = require('path'),
     methodOverride = require('method-override'),
     moment = require('moment'),
     session = require('express-session'),
+    passport = require('passport'),
     redisStore = require('connect-redis')(session);
     
 
@@ -34,6 +35,9 @@ module.exports = function() {
     
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
+
+    app.use(passport.initialize());
+    app.use(passport.session());
     
     require('../app/routes/index.server.routes.js')(app);
     require('../app/routes/users.server.routes.js')(app);
